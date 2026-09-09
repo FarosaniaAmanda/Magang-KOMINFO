3d1a4b6588f49ccb19088f70240c087ae5ecd398
husnatuladibah <husnatul.adibah783@gmail.com>
2026-09-09 07:49:24 +0700

    ubah banyak

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\InformasiController;
+use App\Http\Controllers\Admin\AdminAuthController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\AgendaController;
 
 Route::get('/', [HomeController::class, 'index'])
     ->name('home');
@@ -12,6 +15,28 @@ Route::get('/informasi', [InformasiController::class, 'index'])
 
 Route::get('/informasi/{id}', [InformasiController::class, 'show'])
     ->name('informasi.show');
+
+    // Login Admin
+Route::get('/login-admin', [AdminAuthController::class, 'showLogin'])
+    ->name('admin.login');
+
+Route::post('/login-admin', [AdminAuthController::class, 'login'])
+    ->name('admin.login.process');
+
+
+// Halaman Admin
+Route::middleware('admin')->prefix('admin')->name('admin.')->group(function () {
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
+
+    Route::post('/logout', [AdminAuthController::class, 'logout'])
+        ->name('logout');
+
+    // Agenda
+    Route::resource('agenda', AgendaController::class);
+
+});
     Route::get('/agenda', function () {
     return view('agenda.index');
 })->name('agenda.index');
