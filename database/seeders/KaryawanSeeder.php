5f2be1393fca50d163d3b8e70818f0c5f510f9ee
FarosaniaAmanda <farosaniaamanda60@gmail.com>
2026-09-14 08:38:16 +0700

    Tambah data karyawan

--- database/seeders/KaryawanSeeder.php
+++ database/seeders/KaryawanSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class KaryawanSeeder extends Seeder
 {
@@ -12,6 +12,25 @@ class KaryawanSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('karyawans')->insert([
+            [
+                'nama' => 'aditia s.kom',
+                'nip' => '12345678',
+                'jabatan' => 'staff',
+                'bidang' => 'E-Government',
+                'foto' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Farosania Amanda S.Kom',
+                'nip' => '23456789',
+                'jabatan' => 'staff',
+                'bidang' => 'IKP',
+                'foto' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
