a6aaa55a5ca9766862df9ef125fb56de24785ccf
FarosaniaAmanda <farosaniaamanda60@gmail.com>
2026-09-14 08:31:59 +0700

    Tambah data kepala dinas

--- database/seeders/KepalaDinasSeeder.php
+++ database/seeders/KepalaDinasSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class KepalaDinasSeeder extends Seeder
 {
@@ -12,6 +12,14 @@ class KepalaDinasSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('kepala_dinas')->insert([
+            'nama' => 'ANDRIYANTO S.SIT, MM',
+            'nip' => '12345678910',
+            'jabatan' => 'Kepala Dinas Komunikasi, Informasi Dan Statistik',
+            'deskripsi' => 'Profil Kepala Dinas',
+            'foto' => null,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
